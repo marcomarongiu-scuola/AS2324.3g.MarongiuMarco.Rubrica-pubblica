0a24d83a713c2ddf937f989990a29c9ea2650f0d
coent <coent@MSI>
2024-02-08 08:38:45 +0100

    Aggiungere i file di progetto.

---
+++ AS2324.3g.MarongiuMarco.es12lab/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AS2324._3g.MarongiuMarco.es12lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
